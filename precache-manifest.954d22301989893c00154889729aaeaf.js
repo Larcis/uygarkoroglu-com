8714d3ebb583482b16b8ec64e48b279d49abcdd7
larcis <uygarkoroglu@outlook.com>
2020-07-19 20:54:51 +0300

    Updates

---
+++ precache-manifest.954d22301989893c00154889729aaeaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464a271fc80fe5c34e8c541d4f05f798",
+    "url": "/uygarkoroglu-com/index.html"
+  },
+  {
+    "revision": "2003dc29092232e48e4b",
+    "url": "/uygarkoroglu-com/static/css/main.45043a2b.chunk.css"
+  },
+  {
+    "revision": "5c309f4f461134ab3db4",
+    "url": "/uygarkoroglu-com/static/js/2.c68c869d.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/uygarkoroglu-com/static/js/2.c68c869d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2003dc29092232e48e4b",
+    "url": "/uygarkoroglu-com/static/js/main.2764e2d3.chunk.js"
+  },
+  {
+    "revision": "9928d6009b3b1fd5d4e1",
+    "url": "/uygarkoroglu-com/static/js/runtime-main.7021e1cf.js"
+  },
+  {
+    "revision": "267f11b7e0c53fbe39a3da7a701410fa",
+    "url": "/uygarkoroglu-com/static/media/astar.267f11b7.png"
+  },
+  {
+    "revision": "2070f812d6a581d5782c2df2f13165e8",
+    "url": "/uygarkoroglu-com/static/media/bg2.2070f812.gif"
+  },
+  {
+    "revision": "8b7f342a410f8c4316f0249ec42cb61c",
+    "url": "/uygarkoroglu-com/static/media/latinsquare.8b7f342a.JPG"
+  },
+  {
+    "revision": "377176421c8a23ef327c54674e5166ba",
+    "url": "/uygarkoroglu-com/static/media/mazesolve.37717642.JPG"
+  },
+  {
+    "revision": "041bdec727a3a509c414cbbc204bfb90",
+    "url": "/uygarkoroglu-com/static/media/me.041bdec7.jpeg"
+  },
+  {
+    "revision": "edcfec890faf6bd22748976e0256fa2d",
+    "url": "/uygarkoroglu-com/static/media/tictactoe.edcfec89.png"
+  }
+]);
